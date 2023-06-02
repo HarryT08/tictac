649007c599677fa579ec4ca2f7b1d9a96396eb56
Diego-Pedrozo <diegopedrozovillarreal@gmail.com>
2023-06-02 16:30:02 -0500

    Cambio imagenes login

--- client/src/features/directivos/pages/Herramientas.tsx
+++ client/src/features/directivos/pages/Herramientas.tsx
@@ -1,26 +1,14 @@
-import { useEffect } from "react";
-import { Route, Routes, useLocation } from "react-router";
-import { Dashboard } from "@/components";
+import { Header } from "@/components";
+import { Tabs } from "@/features/directivos/components";
 
 const Herramientas = () => {
-  const location = useLocation();
-
-  useEffect(() => {
-    const htmlElement = document.querySelector("html");
-    if (htmlElement) {
-      // Verificación de nulidad
-      htmlElement.style.scrollBehavior = "auto";
-      window.scroll({ top: 0 });
-      htmlElement.style.scrollBehavior = "";
-    }
-  }, [location.pathname]); // triggered on route change
-
   return (
-    <>
-      <Routes>
-        <Route path="/*" element={<Dashboard />} />
-      </Routes>
-    </>
+    <div>
+      <Header titulo="Herramientas" subtitulo="Listado"/>
+      <div className="mt-3">
+        <Tabs />
+      </div>
+    </div>
   );
 };
 
